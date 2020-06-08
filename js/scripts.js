3ee83cc0a9c3d8c45c1fb40d2f7b0d3eabac45ff
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-08 13:13:46 -0700

    add forEach loops for practice, update readme
    co-authored-by:  Ester Tataje <ester.tataje@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  friends.forEach(function(friend) {
+    alert(friend);
+  })
+  let sum = 0;
+  numbers.forEach(function(num) {
+    sum += num;
+  })
+  alert(sum);
 });
+
+const friends = ["Chris", "Alaina", "James", "Skip"];
+const numbers = [42, 10, 22, 49, 68, 33];
